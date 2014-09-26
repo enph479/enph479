7a8f20c3976991bcf7f654ca89e8e128dc5a8ed9
Blake <blake.raymond@aquaticinformatics.com>
2014-09-25 22:26:58 -0700

    Add solution and projects for MECoordination, add selectable tree from CodeProject, sample extension that uses selectable tree

---
+++ src/MECoordination.UI/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Input;
+using MECoordination.UI;
+
+namespace ElectricalToolSuite.MECoordination.UI
+{
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+
+            SelectableTreeViewModel root = this.tree.Items[0] as SelectableTreeViewModel;
+
+            base.CommandBindings.Add(
+                new CommandBinding(
+                    ApplicationCommands.Undo,
+                    (sender, e) => // Execute
+                    {
+                        e.Handled = true;
+                        root.IsChecked = false;
+                        this.tree.Focus();
+                    },
+                    (sender, e) => // CanExecute
+                    {
+                        e.Handled = true;
+                        e.CanExecute = (root.IsChecked != false);
+                    }));
+
+            this.tree.Focus();
+        }
+
+        public IEnumerable<string> SelectedItems
+        {
+            get
+            {
+                var s = new Stack<SelectableTreeViewModel>();
+
+                foreach (var item in tree.Items)
+                    s.Push((SelectableTreeViewModel) item);
+
+                while (s.Any())
+                {
+                    var vm = s.Pop();
+
+                    if (vm.Children.Any())
+                    {
+                        foreach (var c in vm.Children)
+                            s.Push(c);
+                        continue;
+                    }
+
+                    if ((vm.IsChecked.HasValue && vm.IsChecked.Value) || (!vm.IsChecked.HasValue && vm.IsInitiallySelected))
+                        yield return vm.Name;
+                }
+            }
+        }
+    }
+}
---
+++ src/MECoordination/ExternalCommand.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using System.Text;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace ElectricalToolSuite.MECoordination
+{
+    [Transaction(TransactionMode.Automatic)]
+    public class ExternalCommand : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            var wnd = new UI.Window1();
+            wnd.ShowDialog();
+
+            var selected = wnd.SelectedItems.ToList();
+
+            var sb = new StringBuilder();
+
+            sb.AppendLine("You selected:");
+            foreach (var item in selected)
+            {
+                sb.AppendLine(item);
+            }
+
+            TaskDialog.Show("Selected items", sb.ToString());
+
+            return Result.Succeeded;
+        }
+    }
+}
