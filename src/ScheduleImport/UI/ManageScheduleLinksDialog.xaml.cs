4a66d42babeedb2d169bb0ee0db4308afb7c1d79
Blake Raymond <blakeraymond.ubc@gmail.com>
2015-02-14 21:27:56 -0800

    Remove some gunk from ManageScheduleLinksDialog

--- src/ScheduleImport/UI/ManageScheduleLinksDialog.xaml.cs
+++ src/ScheduleImport/UI/ManageScheduleLinksDialog.xaml.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Diagnostics;
-using System.Linq;
+﻿using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using Autodesk.Revit.DB;
-using Autodesk.Revit.DB.Electrical;
 using Autodesk.Revit.UI;
 
 namespace ElectricalToolSuite.ScheduleImport.UI
@@ -16,14 +13,12 @@ namespace ElectricalToolSuite.ScheduleImport.UI
     public partial class ManageScheduleLinksDialog
     {
         private readonly Document _document;
-        private readonly UIDocument _uiDocument;
 
         public bool PressedCreate { get; private set; }
 
         public ManageScheduleLinksDialog(Document document, UIDocument uiDocument)
         {
             _document = document;
-            _uiDocument = uiDocument;
             InitializeComponent();
         }
 
@@ -33,7 +28,6 @@ namespace ElectricalToolSuite.ScheduleImport.UI
 
             if (hasSelection)
             {
-                //                ReloadButton.IsEnabled = true;
                 EditButton.IsEnabled = true;
                 RemoveButton.IsEnabled = true;
                 ReloadButton.IsEnabled =
@@ -66,23 +60,10 @@ namespace ElectricalToolSuite.ScheduleImport.UI
             var selectedLink = (ManagedScheduleLink) ManagedScheduleLinksDataGrid.SelectedItem;
 
             selectedLink.Reload();
-
-//            var schedule = selectedLink.GetSchedule();
-//            var workbookPath = selectedLink.WorkbookPath;
-//            var worksheetName = selectedLink.WorksheetName;
-//
-//            Cursor = Cursors.Wait;
-//            ExternalCommand.ImportSchedule(schedule, workbookPath, worksheetName);
-//            Cursor = Cursors.Arrow;
         }
 
         private void EditButton_Click(object sender, RoutedEventArgs e)
         {
-            string workbookPath;
-            string worksheetName;
-            string scheduleName;
-            string scheduleType;
-
             var selectedLink = (ManagedScheduleLink)ManagedScheduleLinksDataGrid.SelectedItem;
 
             var wnd = new SheetSelectionDialog(_document);
@@ -96,18 +77,15 @@ namespace ElectricalToolSuite.ScheduleImport.UI
 
             wnd.ValidName = selectedLink.ScheduleName;
 
-//                wnd.ScheduleNameTextBox.IsEnabled = false;
-//                wnd.ScheduleTypeTextBox.IsEnabled = false;
-
             wnd.Title = "Edit Excel Schedule Link";
 
                 if (wnd.ShowDialog() != true)
                     return;
 
-                workbookPath = wnd.FilePathTextBox.Text;
-                worksheetName = (string)wnd.SheetComboBox.SelectedItem;
-                scheduleName = wnd.ScheduleNameTextBox.Text;
-                scheduleType = wnd.ScheduleTypeTextBox.Text;
+                var workbookPath = wnd.FilePathTextBox.Text;
+                var worksheetName = (string)wnd.SheetComboBox.SelectedItem;
+                var scheduleName = wnd.ScheduleNameTextBox.Text;
+                var scheduleType = wnd.ScheduleTypeTextBox.Text;
 
             selectedLink.WorkbookPath = workbookPath;
             selectedLink.WorksheetName = worksheetName;
@@ -128,12 +106,7 @@ namespace ElectricalToolSuite.ScheduleImport.UI
             LinkGateway.DeleteLink(schedule);
             _document.Delete(schedule.Id);
 
-//            var schema = ExternalCommand.GetSchema();
-//            Debug.Assert(schedule.DeleteEntity(schema));
-
-//            Cursor = Cursors.Wait;
             ManagedScheduleLinksDataGrid.ItemsSource = ExternalCommand.GetManagedSchedules(_document);
-//            Cursor = Cursors.Arrow;
 
             ManagedScheduleLinksDataGrid.Items.Refresh();
             UpdateButtons();
@@ -154,13 +127,6 @@ namespace ElectricalToolSuite.ScheduleImport.UI
             PressedCreate = true;
             DialogResult = true;
             UpdateButtons();
-//            Visibility = System.Windows.Visibility.Collapsed;
-//
-//            var selectedPanel = new ElementSelector(_uiDocument).SelectSingle() as FamilyInstance;
-//
-//            Debug.Assert(selectedPanel != null);
-//
-//            Visibility = System.Windows.Visibility.Visible;
         }
 
         private void ReloadAllButton_Click(object sender, RoutedEventArgs e)
